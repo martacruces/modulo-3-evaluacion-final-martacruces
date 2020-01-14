d6088ca10609d2c333864d2aaef85a469277a85a
martacruces <martacrucesdiaz@gmail.com>
2020-01-14 16:38:30 +0100

    Updating docs

---
+++ docs/precache-manifest.f4259c69d99c2f2bb84100883f778316.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "922829bb5e4a61fafa4b2047e0be9006",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e6a0474ef647ad7be73f",
+    "url": "./static/css/main.bda6ad98.chunk.css"
+  },
+  {
+    "revision": "571ecc82c1b6a8153e17",
+    "url": "./static/js/2.e71cd435.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.e71cd435.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e6a0474ef647ad7be73f",
+    "url": "./static/js/main.2c032ed0.chunk.js"
+  },
+  {
+    "revision": "18bbdebd87877c140adc",
+    "url": "./static/js/runtime-main.6d194c11.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/Logo.75509f2e.png"
+  },
+  {
+    "revision": "34f79f3993ad8c1cfe653eab557d5629",
+    "url": "./static/media/background-galaxy.34f79f39.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.61d260d40a32b64e541c0f9fc4ddce01.js"
+  "./precache-manifest.f4259c69d99c2f2bb84100883f778316.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
