9d3888bfc8b91688c77ce0c60599f5df8b45fe8d
martacruces <martacrucesdiaz@gmail.com>
2020-01-14 16:32:27 +0100

    Create docs

---
+++ docs/precache-manifest.61d260d40a32b64e541c0f9fc4ddce01.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71abfdf91ed84443b5cf0c476719a24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "197627877a7606a9d271",
+    "url": "/static/css/main.ad39c6a1.chunk.css"
+  },
+  {
+    "revision": "571ecc82c1b6a8153e17",
+    "url": "/static/js/2.e71cd435.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.e71cd435.chunk.js.LICENSE"
+  },
+  {
+    "revision": "197627877a7606a9d271",
+    "url": "/static/js/main.2c032ed0.chunk.js"
+  },
+  {
+    "revision": "999e0c3fe91f04b19ba6",
+    "url": "/static/js/runtime-main.1f301541.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "/static/media/Logo.75509f2e.png"
+  },
+  {
+    "revision": "34f79f3993ad8c1cfe653eab557d5629",
+    "url": "/static/media/background-galaxy.34f79f39.png"
+  }
+]);
